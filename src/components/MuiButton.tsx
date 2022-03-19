6a3629686a5f3d34c6512e2c6ce756c72e1a2da0
Suraj SK <surajsk2002@gmail.com>
2022-03-19 12:53:55 +0530

    MuiButton - initial

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material"
+export const MuiButton = () => {
+  return (
+    <div>
+        <Button variant="text">Text Button</Button>
+        <Button variant="contained">Contained Button</Button>
+        <Button variant="outlined">Outlined Button</Button>
+    </div>
+  )
+}
