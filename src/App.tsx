1e86554e1884860ed20362c2e27eb064602059d0
Suraj SK <surajsk2002@gmail.com>
2022-03-19 12:59:48 +0530

    MuiButton commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import { MuiButton } from './components/MuiButton';
-import { MuiTypography } from './components/MuiTypography';
+// import { MuiTypography } from './components/MuiTypography';
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
+    {/* <MuiTypography /> */}
       <MuiButton />
     </div>
   );
--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@mui/material"
+import { Stack } from "@mui/material"
 export const MuiButton = () => {
   return (
-    <div>
+    <Stack spacing={2} direction='row'>
         <Button variant="text">Text Button</Button>
         <Button variant="contained">Contained Button</Button>
         <Button variant="outlined">Outlined Button</Button>
-    </div>
+    </Stack>
   )
 }
