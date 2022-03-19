870d4cca6ce5636fa2b9600b9f213c28ce5949bc
Suraj SK <surajsk2002@gmail.com>
2022-03-19 13:14:27 +0530

    MuiButton commit 3

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,11 +1,40 @@
 import { Button } from "@mui/material"
 import { Stack } from "@mui/material"
+
 export const MuiButton = () => {
   return (
+    <Stack spacing={4}>
+        <Stack spacing={2} direction='row'>
+            <Button variant="text" href='https://github.com/sksuraj17'>GitHub Text</Button>
+            <Button variant="contained" href='https://github.com/sksuraj17'>GitHub Contained </Button>
+            <Button variant="outlined" href='https://github.com/sksuraj17'>gitHub Outlined </Button>
+        </Stack>
+
+
     <Stack spacing={2} direction='row'>
-        <Button variant="text">Text Button</Button>
-        <Button variant="contained">Contained Button</Button>
-        <Button variant="outlined">Outlined Button</Button>
+        <Button variant='contained' color='primary'>Primary</Button>
+        <Button variant='contained' color='secondary'>Secondary</Button>
+        <Button variant='contained' color='error'>Error</Button>
+        <Button variant='contained' color='warning'>Warning</Button>
+        <Button variant='contained' color='info'>Info</Button>
+        <Button variant='contained' color='success'>Success</Button>
+    </Stack>
+    <Stack spacing={2} direction='row'>
+        <Button variant='text' color='primary'>Primary</Button>
+        <Button variant='text' color='secondary'>Secondary</Button>
+        <Button variant='text' color='error'>Error</Button>
+        <Button variant='text' color='warning'>Warning</Button>
+        <Button variant='text' color='info'>Info</Button>
+        <Button variant='text' color='success'>Success</Button>
+    </Stack>
+    <Stack spacing={2} direction='column'>
+        <Button variant='outlined' color='primary'>Primary</Button>
+        <Button variant='outlined' color='secondary'>Secondary</Button>
+        <Button variant='outlined' color='error'>Error</Button>
+        <Button variant='outlined' color='warning'>Warning</Button>
+        <Button variant='outlined' color='info'>Info</Button>
+        <Button variant='outlined' color='success'>Success</Button>
+    </Stack>
     </Stack>
   )
 }
